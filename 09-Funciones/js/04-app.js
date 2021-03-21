a53dca9cb0d1008a5d38041bf82d6f9f3eacf387
robertoezmedina <robertoezmedina@gmail.com>
2021-03-21 03:12:04 -0600

    diferencias entre funciones y metodos

---
+++ 09-Funciones/js/04-app.js
@@ -0,0 +1,7 @@
+// diferencias entre funciones y metodos
+const num1 = 20; // numero 20 en una variable de tipo number por su valor
+const num2 = '20'; // numero 20 en una variable de tipo string por su valor
+
+console.log(parseInt(num2)); // esto es una funcion
+
+console.log(num1.toString()); // esto es un metodo
