ec099c06923ed10bd37ed89dadd0baa5167fe1b5
robertoezmedina <robertoezmedina@gmail.com>
2021-03-21 02:48:51 -0600

    creacion de funciones declarativas y de expresion

---
+++ 09-Funciones/js/01-app.js
@@ -0,0 +1,14 @@
+// crear funciones en js - function expression y function declaration
+// function declaration
+function sumar(){ // funciion encargada de mostrar por consola una suma de dos valores
+    console.log(2+2);
+}
+
+sumar(); // llamada a la funcion - muestra como resultado la suma de 2 + 2
+
+// function expression
+const sumar2 = function(){ // funcion encargada de mostrar por consola la suma de dos valores
+    console.log(3+3);
+}
+
+sumar2(); // llamada a la funcion - muestra como resultado la suma de 3 + 3
