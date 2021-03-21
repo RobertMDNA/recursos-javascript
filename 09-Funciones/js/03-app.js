d38aa746afed636d950f25bdc62778171f3b97b5
robertoezmedina <robertoezmedina@gmail.com>
2021-03-21 03:06:27 -0600

    algunas funciones nativas en js

---
+++ 09-Funciones/js/03-app.js
@@ -0,0 +1,6 @@
+// algunas funciones nativas en javascript
+alert('Hubo un error'); // funcion predefinida que muestra una ventana emergente en el navegador
+
+prompt('¿Cual es tu edad?'); // funcion predefinida que muestra una ventana emergente en el navegador para capturar datos desde el teclado
+
+console.log(parseInt('20')); // funcion predefinida que permite convertir un valor string a un valor number
