e54e31f67a4c19c91eb5c46fc23edaf6d80b04d7
robertoezmedina <robertoezmedina@gmail.com>
2021-03-26 14:03:18 -0600

    comparando mayor o menor que

---
+++ 10-Estructuras/js/03-app.js
@@ -0,0 +1,10 @@
+// comparando mayor o menor que
+const dinero = 500;
+const totalPagar = 300;
+
+// comprando que el dinero sea mayor al total a pagar para poder comprar
+if (dinero >= totalPagar) {
+    console.log('Si puedes pagar');
+} else {
+    console.log('No puedes pagar');
+}
