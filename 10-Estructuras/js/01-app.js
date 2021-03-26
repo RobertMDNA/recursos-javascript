ad7a772e5dd0662888192b8f6573c3f4548c02ae
robertoezmedina <robertoezmedina@gmail.com>
2021-03-25 22:41:12 -0600

    creando un condicional if

---
+++ 10-Estructuras/js/01-app.js
@@ -0,0 +1,9 @@
+// creando un condicional if
+const puntaje = 1000;
+
+if (puntaje == 1000) { // condicional if que revisa que el puntaje sea el mismo al que se revisa. Si es igual se ejecuta la siguiente linea
+    console.log('Si es igual'); // se ejecutara este clg en caso de ser verdadera la condicion
+}else{
+    // en caso de que la condicion regrese false, se ejecutara esta siguiente linea de codigo
+    console.log('No, no es igual');
+}
