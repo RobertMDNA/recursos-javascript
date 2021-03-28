ad927c9d37351f358dd7462bece0bcbdb6c1aa8c
Roberto Carlos Chávez Medina <robertoezmedina@gmail.com>
2021-03-27 22:11:33 -0600

    deteniendo la ejecucion de un condicional if con una funcion

---
+++ 10-Estructuras/js/08-app.js
@@ -0,0 +1,14 @@
+// detener la ejecucion de un condicional if con una function
+const autenticado = true;
+
+if (autenticado) {
+    console.log('Si esta autenticado');
+}
+
+const puntaje = 500;
+
+if (puntaje > 400) {
+    console.log('Excelente Puntaje');
+}else if(puntaje > 300){
+    console.log('Buen Puntaje');
+}
