f3b73be5be78ce4ed52ab86eb4f124e55aa90b56
Roberto Carlos Chávez Medina <robertoezmedina@gmail.com>
2021-03-27 22:28:18 -0600

    operador ternario

---
+++ 10-Estructuras/js/09-app.js
@@ -0,0 +1,9 @@
+// operador ternario
+const autenticado = true;
+const puedePagar = true;
+
+// operador ternario
+console.log(autenticado ? 'Si esta autenticado' : 'No esta autenticado');
+
+// revisando dos o mas condiciones en un operador ternario
+console.log(autenticado && puedePagar ? 'Si puede pagar' : 'No puede pagar');
